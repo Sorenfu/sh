d33b9b6e37226af67ae06fa272e3da9ae2b26095
Sorenfu <sh@compfix.dk>
2015-04-12 16:57:24 +0200

    Initial commit

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,16 @@
+//= require jquery
+//= require jquery_ujs
+//= require jquery.scrollto
+//= require jquery.fittext
+//= require jquery.flexslider
+//= require jquery.easing.1.3
+//= require jquery.label_better
+//= require jquery.easypiechart
+
+//= require turbolinks
+//= require bootstrap
+//= require jquery.masonry
+//= require waypoints
+//= require modernizr.min
+
+//= require meflat
